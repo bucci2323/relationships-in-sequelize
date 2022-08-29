e04bf75c1d4af1ee3f4b71adda39be91464b8d05
bucci2323 <98851839+bucci2323@users.noreply.github.com>
2022-08-29 15:22:24 +0100

    first commit

---
+++ src/user/User.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const Article = require('../article/Article')
+class User extends Model {};
+
+User.init({
+  username: {
+    type: DataTypes.STRING
+  },
+  email: {
+    type: DataTypes.STRING
+  },
+  password: {
+    type: DataTypes.STRING
+  }
+}, {
+  sequelize,
+  modelName: 'user',
+  timestamps: false
+})
+Article.hasMany(Article, {foreignKey: "userId"})
+Article.belongsTo(User);
+
+module.exports = User;
---
+++ src/user/UserService.js
@@ -0,0 +1,54 @@
+const User = require('./User');
+const UserNotFoundException = require('./UserNotFoundException');
+const bcrypt = require('bcrypt');
+const Article = require('../article/Article')
+
+const create = async (body) => {
+  const { username, email, password } = body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  await User.create({ username, email, password: hashedPassword});
+}
+
+const getUsers = async (pagination) => {
+  const { page, size } = pagination;
+
+  const usersWithCount = await User.findAndCountAll({
+    limit: size,
+    offset: page * size,
+    attributes: ['id', 'username', 'email'],
+    
+    include: [
+      {
+        model:Article,
+        as: 'articles'
+      }
+    ]
+
+  });
+  return {
+    content: usersWithCount.rows,
+    totalPages: Math.ceil(usersWithCount.count / Number.parseInt(size))
+  };
+}
+
+const getUser = async (id) => {
+  const user = await User.findOne({
+    where: {id: id},
+    attributes: ['id', 'username', 'email']
+  });
+  if(!user) {
+    throw new UserNotFoundException();
+  }
+  return user;
+}
+
+const findByEmail = async (email) => {
+  return await User.findOne({where: {email: email}});
+}
+
+module.exports = {
+  create,
+  getUsers,
+  getUser,
+  findByEmail
+}
